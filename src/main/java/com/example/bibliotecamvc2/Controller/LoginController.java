367c6b597fcae947f31cfc343795dacc9617bb00
eloy torres <20253rd037@utez.edu.mx>
2026-04-17 15:21:57 -0600

    correcciones main echas

--- src/main/java/com/example/bibliotecamvc2/Controller/LoginController.java
+++ src/main/java/com/example/bibliotecamvc2/Controller/LoginController.java
@@ -2,7 +2,14 @@ package com.example.bibliotecamvc2.Controller;
 
 import com.example.bibliotecamvc2.Model.UserModel;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -11,25 +18,39 @@ public class LoginController {
     protected TextField txtUsuario;
     @FXML
     protected TextField txtPassword;
+    @FXML
+    protected Label lbl;
 
     @FXML
-    public void onVerificar() {
+    public void validarUsuario() {
 
         String usuario = txtUsuario.getText();
         String password = txtPassword.getText();
 
         if (usuario.isEmpty() || password.isEmpty()) {
-            lbMensaje.setText("Llena todos los campos");
+            lbl.setText("Llena todos los campos");
             return;
         }
 
         boolean valido = userModel.validarUsuario(usuario, password);
 
         if (valido) {
-            lbMensaje.setText("Bienvenido " + usuario);
+            lbl.setText("Bienvenido " + usuario);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("segundaVista.fxml"));
+            try {
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setTitle("segunda vista");
+                stage.setScene(new Scene(root));
+                stage.show();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
 
         } else {
-            lbMensaje.setText("Datos incorrectos");
+            lbl.setText("Datos incorrectos");
         }
     }
 }
---
+++ src/main/java/com/example/bibliotecamvc2/Controller/Menu.java
@@ -0,0 +1,4 @@
+package com.example.bibliotecamvc2.Controller;
+
+public class Menu {
+}
